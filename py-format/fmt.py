92de864ce2c0f19c5220f7a3ca11d1b8c227cac4
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2023-04-22 15:04:53 +0200

    Add python print format module

---
+++ py-format/fmt.py
@@ -0,0 +1,113 @@
+# Basic python module providing functions to output
+# formatted text using ANSI escape sequences.
+
+# Author : Côme VINCENT, 2023
+
+def clear() -> None:
+    """Clears the terminal screen, deletes scrollback buffer and
+    moves the cursor to the top left corner of the screen."""
+    print("\x1b[3J\x1b[H\x1b[J", end="")
+
+
+def pitalic(*args, **kwargs) -> None:
+    """Prints given arguments in italic. Stdout unless specified."""
+    print("\x1b[3m", end="")
+    print(*args, **kwargs)
+    print("\x1b[23m", end="")
+
+
+def pbold(*args, **kwargs) -> None:
+    """Prints given arguments in bold. Stdout unless specified."""
+    print("\x1b[1m", end="")
+    print(*args, **kwargs)
+    print("\x1b[22m", end="")
+
+
+def perror(*args, **kwargs) -> None:
+    """Prints given arguments in bold red. Stdout unless specified."""
+    print("\x1b[31m", end="")
+    print(*args, **kwargs)
+    print("\x1b[39m", end="")
+
+
+def pblink(s: str, **kwargs) -> None:
+    """Prints a string in blink. Stdout unless specified."""
+    print("\x1b[5m" + s + "\x1b[25m", **kwargs)
+
+
+def bool_input(prompt: str) -> bool:
+    """Asks the user to input a boolean value.
+    Cancel with Ctrl+C."""
+
+    # Get the user input
+    bold_prompt = "\x1b[1m" + prompt + "\x1b[0m"
+    try:
+        str_input = input(bold_prompt).lower()
+    except KeyboardInterrupt:
+        print()
+        exit(0)
+
+    # Check if the input is valid
+    bool_input = ['true', '1', 't', 'y', 'yes', 'o',
+                  'false', '0', 'f', 'n', 'no', 'n']
+
+    while True:
+        try:
+            if bool_input.index(str_input) < 6:
+                return True
+            else:
+                return False
+        except ValueError:
+            try:
+                str_input = input(bold_prompt).lower()
+            except KeyboardInterrupt:
+                print()
+                exit(0)
+
+
+def print_table(data: list[str], headers: list[str]) -> None:
+    """Shows a formatted table in the terminal."""
+    max_length_column = [0] * len(headers)
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if len(str(data[i][j])) > max_length_column[j]:
+                max_length_column[j] = len(str(data[i][j]))
+
+    print("|", end="")
+    for i, header in enumerate(headers):
+        if len(header) > max_length_column[i]:
+            max_length_column[i] = len(header)
+        pbold(" " + header.ljust(max_length_column[i]) + " |", end="")
+    print()
+
+    print("*" * (sum(max_length_column) + 3 * len(headers) + 1))
+
+    for row in data:
+        print("|", end="")
+        for i in range(len(row)):
+            print(" " + str(row[i]).ljust(max_length_column[i]) + " |", end="")
+        print()
+
+
+def form_input(inputs: list) -> list[str]:
+    """Asks the user to input a list of values."""
+    answers = [None] * len(inputs)
+
+    # Calcul des longueurs des labels
+    inputs = [i + " : " for i in inputs]
+    label_lengths = [len(i) for i in inputs]
+    n_labels = len(inputs)
+
+    # Affichage des labels
+    pbold(*inputs, sep="\n", end="")
+
+    # Affichage des champs de saisie
+    print(f"\x1b[{n_labels - 1}F", end="")
+    for i in range(n_labels):
+        print(f"\x1b[{label_lengths[i]}C", end="")
+        try:
+            answers[i] = input()
+        except KeyboardInterrupt:
+            clear()
+            return None
+    return answers
---
+++ py-format/readme.md
@@ -0,0 +1,99 @@
+# Python Module: Text Display Functions
+
+This Python module contains a collection of functions for displaying text in various formats, such as bold, italic, and error messages. It also includes functions for getting user input and displaying data in a table format.
+It heavily relies on ANSI escape sequences.
+
+## Functions
+
+Here is a list of functions included in this module:
+
+### clear()
+
+- Description: Clears the screen.
+- Usage: `clear()`
+
+### pitalic(\*args, \*\*kwargs)
+
+- Description: Displays *args in italic. \*\*kwargs will be passed to python's print function.
+- Usage: `pitalic("Your Text Here")` or `pitalic(*list_to_unpack)` etc.
+- Output : *Your Text Here*
+
+### pbold(\*args, \*\*kwargs)
+
+- Description: Displays *args in bold. \*\*kwargs will be passed to python's print function.
+- Usage: `pbold("Your Text Here")` or `pbold(*list_to_unpack)` etc.
+- Output : **Your Text Here**
+
+### perror(\*args, \*\*kwargs)
+
+- Description: Displays *args in red and bold. \*\*kwargs will be passed to python's print function.
+- Usage: `perror("Your Text Here")` or `perror(*list_to_unpack)` etc.
+- Output : <span style="color:red">**Your Text Here**</span>
+
+### pblink(\*args, \*\*kwargs)
+
+- Description: Displays *args in blinking. \*\*kwargs will be passed to python's print function.
+- Usage: `pblink("Your Text Here")` or `pblink(*list_to_unpack)` etc.
+- Output : Your Text Here (but actually blinking)
+
+### bool_input(prompt: str) -> bool
+
+- Description: Asks the user to enter a boolean value. Can parse regular bool inputs like `y`, `n`, `true`, `false`, etc.
+- Usage: `user_input = bool_input("Enter True or False: ")`
+
+### print_table(data: list, headers: list)
+
+- Description: Displays the data in a table format.
+- Usage: 
+    ```python
+    data = [['John', 28], ['Jane', 25]]
+    headers = ['Name', 'Age']
+    print_table(data, headers)
+    ```
+- Output:
+  ```
+    | Name | Age |
+    **************
+    | John | 28  |
+    | Jane | 25  |
+  ```
+
+### print_as_form(inputs: list) -> list[str]
+
+- Description: Displays a form and returns the user's responses.
+- Usage: 
+    ```python
+    form_questions = ['Name', 'Age', 'City']
+    
+    user_responses = print_as_form(form_questions)
+    ```
+- Output:
+  ```
+    Name: 
+    Age: 
+    City: 
+  ```
+
+## Combined example
+
+```python
+from fmt import form_input, print_table
+headers = ['Name', 'Age', 'Location']
+data = []
+while True:
+    user = fmt.form_input(headers)
+    if user is None:
+        break
+    else:
+        data += [user]
+fmt.print_table(data, headers)
+```
+If user inputs A, B, C, ... L, the output will be:
+```
+| Name | Age | Location |
+*************************
+| A    | B   | C        |
+| D    | E   | F        |
+| G    | H   | I        |
+| J    | K   | L        |
+```
